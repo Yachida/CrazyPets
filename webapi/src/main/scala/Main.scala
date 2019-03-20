de1a284b82a5febfe4eaaf44ff36b192bfb7fd93
IzumiSy <beetle-noise@gmx.com>
2019-03-20 16:10:57 +0900

    csv

--- webapi/src/main/scala/Main.scala
+++ webapi/src/main/scala/Main.scala
@@ -1,19 +1,31 @@
+import java.io.File
+
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.server.Directives._
 import akka.stream.ActorMaterializer
 import spray.json.DefaultJsonProtocol
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
+
 import scala.io.StdIn
+import com.github.tototoshi.csv._
+
+import scala.util.{Failure, Success, Try}
 
 case class PetFace(eye_left_x: Float, eye_left_y: Float, eye_right_x: Float, eye_right_y: Float,
-                   nose_x: Float, nose_y: Float, mouth_x: Float, mouth_y: Float, score: Float)
+                   nose_x: Float, nose_y: Float, mouth_x: Float, mouth_y: Float, score: Float) {
+  def toList = {
+    List(eye_left_x, eye_left_y, eye_right_x, eye_right_y, nose_x, nose_y, mouth_x, mouth_y, score)
+  }
+}
 
 trait JsonSupport extends SprayJsonSupport with DefaultJsonProtocol {
   implicit val petFaceFormat = jsonFormat9(PetFace.apply)
 }
 
 object Main extends JsonSupport {
+  val CSV_FILE = "/var/hoge.csv"
+
   def main(args: Array[String]) = {
     implicit val system = ActorSystem("my-system")
     implicit val materializer = ActorMaterializer()
@@ -22,16 +34,22 @@ object Main extends JsonSupport {
     val route =
       path("pet") {
         get {
+          // TODO: dummyなのであとでつくる
           val v = PetFace(0,0,0,0,0,0,0,0,0)
-          complete(
-            Seq(v, v, v, v, v)
-          )
+          complete(Seq(v, v, v, v, v))
         }
       } ~
       path("pet") {
         post {
           entity(as[Seq[PetFace]]) { request =>
-            complete("POST hoge")
+            Try {
+              val writer = CSVWriter.open(new File("/var/hoge.csv"), append = true)
+              request.foreach(r => writer.writeRow(r.toList))
+              writer.close()
+            } match {
+              case Success(_) => complete("ok")
+              case Failure(e) => complete(s"Failed: ${e.getMessage}")
+            }
           }
         }
       }
