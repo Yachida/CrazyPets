7562dbae7a05983a3f22d368633e660555425bf4
seki <seki@fringe81.com>
2019-03-21 12:09:18 +0900

    サーバサイドとのつなぎ込み想定コード

---
+++ client/public/scripts/home.js
@@ -0,0 +1,50 @@
+/* ====== Home ====== */
+
+function renderHome() {
+  // 対象ランクはランダムに選定
+  const targetRank = Math.floor(Math.random() * 5) + 1;
+  console.log('targetRank', targetRank);
+
+  const message = document.getElementsByClassName('main_title')[0];
+  message.textContent = (() => {
+    if (targetRank === 1) {
+      return "出てけー！！！！！（でもまた来てね。）"
+    } else if (targetRank === 2) {
+      return "月曜の朝6時の気分です。また来てほしいぞ。"
+    } else if (targetRank === 3) {
+      return "居るのは別にいいけど、構ってやれないぞ。"
+    } else if (targetRank === 4) {
+      return "飯でも行こか。今日は魚の気分。"
+    } else if (targetRank === 5) {
+      return "遊びに来てくれたの！？何する！？何する！？駅伝！？"
+    }
+  })();
+
+
+
+
+
+  const topJson =
+    JSON.parse(window.gotData)
+      .filter(data => data.rank);
+
+  const creature = !!topJson ? CreatureDOM(JSON.parse(topJson)) : document.createElement('div');
+
+  document.getElementById('container').appendChild(creature);
+}
+
+/* ====== Main ====== */
+
+function main() {
+  if (!!window.gotData) {
+    renderHome()
+  } else {
+    setTimeout(() => {
+      main();
+    }, 100);
+  }
+}
+
+window.addEventListener("load", () => {
+  main();
+});
--- client/public/scripts/train.js
+++ client/public/scripts/train.js
@@ -19,13 +19,39 @@ function renderTrain() {
   // 全部選択完了した時の処理
   if (selectedIndexes.length === 5) {
 
+    // データ送信
+    const dataForSend =
+      [
+        mockJson[selectedIndexes[0]],
+        mockJson[selectedIndexes[1]],
+        mockJson[selectedIndexes[2]],
+        mockJson[selectedIndexes[3]],
+        mockJson[selectedIndexes[4]]
+      ]
+        .map((data, i) => {
+          return {...data, rank: 5 - i}
+        });
+
+    $.ajax({
+      url: 'http://crazy-pets.grouchydev.io/api',
+      type: 'post',
+      data: dataForSend
+    })
+      .done(() => {
+        console.log('送信完了！')
+      })
+      .fail(() => {
+        console.log('送信エラー！')
+      });
+
     const topIndex = selectedIndexes[0];
-    const dataJson = JSON.stringify(mockJson[topIndex]);
-    localStorage.setItem('topData', dataJson);
+
+    const topDataJson = JSON.stringify(mockJson[topIndex]);
+    localStorage.setItem('topData', topDataJson);
 
     setTimeout(() => {
       location.href = 'thanks.html';
-    }, 1000)
+    }, 100000)
   }
 }
 
@@ -58,9 +84,9 @@ function TrainDOM(data, index) {
   rankText.textContent =
     isSelected
       ? selectedIndexes
-      .map((selectedIndex, rankMinus1) => [selectedIndex, rankMinus1 + 1])
-      .filter(tuple => tuple[0] === index)
-      .map(([selectedIndex, rank]) => rank)[0]
+        .map((selectedIndex, rankMinus1) => [selectedIndex, rankMinus1 + 1])
+        .filter(tuple => tuple[0] === index)
+        .map(([selectedIndex, rank]) => rank)[0]
       : '';
 
   // 表示/非表示
@@ -82,8 +108,16 @@ function onSelect(index) {
 
 
 /* ====== Main ====== */
+function main() {
+  if (!!window.gotData) {
+    renderTrain()
+  } else {
+    setTimeout(() => {
+      main();
+    }, 100);
+  }
+}
 
 window.addEventListener("load", () => {
-  // outputMockJson();
-  renderTrain();
+  main();
 });
